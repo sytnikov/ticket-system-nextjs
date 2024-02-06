f027182c466c5aa0e50fbf0185a2b7f9a3f0ddcc
sytnikov <sytnikov.alexey@gmail.com>
2024-02-06 19:24:45 +0200

    added dynamic segments for separate tickets

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,30 +1,34 @@
+import Link from "next/link";
+
 async function getTickets() {
   const res = await fetch("http://localhost:4000/tickets", {
     next: {
-      revalidate: 0
-    }
-  })
+      revalidate: 0,
+    },
+  });
 
-  return res.json()
+  return res.json();
 }
 
 export default async function TicketList() {
-  const tickets = await getTickets()
-  
+  const tickets = await getTickets();
+
   return (
     <>
       {tickets.map((ticket) => (
-        <div key={ticket.id} className="card my-5">
-          <h3>{ticket.title}</h3>
-          <p>{ticket.body.slice(0, 200)}...</p>
-          <div className={`pill ${ticket.priority}`}> 
-            {ticket.priority} priority
+        <Link href={`/tickets/${ticket.id}`}>
+          <div key={ticket.id} className="card my-5">
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
           </div>
-        </div>
+        </Link>
       ))}
       {tickets.length === 0 && (
         <p className="text-center">There are no open tickets. yay!</p>
       )}
     </>
-  )
+  );
 }
