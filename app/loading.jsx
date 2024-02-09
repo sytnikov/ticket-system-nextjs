cb7bb3da7055ea1fb7f4db64cf03fb3a5d81c91f
sytnikov <sytnikov.alexey@gmail.com>
2024-02-09 12:07:59 +0200

    create ticket form, loading spinner added

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,8 +1,12 @@
 export default function Loading() {
   return (
-    <main className="text-center">
-      <h2 className="text-primary">Loading...</h2>
-      <p><small>Hopefully, not for too long</small></p>
+    <main className="loadingSpinnerContainer">
+      <div className="loadingSpinner"></div>
     </main>
-  )
+
+    // <main className="text-center">
+    //   <h2 className="text-primary">Loading...</h2>
+    //   <p><small>Hopefully, not for too long</small></p>
+    // </main>
+  );
 }
--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,8 +1,5 @@
 import Link from "next/link";
-
 async function getTickets() {
-  await new Promise(resolve => setTimeout(resolve, 3000))
-
   const res = await fetch("http://localhost:4000/tickets", {
     next: {
       revalidate: 0,
@@ -13,21 +10,20 @@ async function getTickets() {
 }
 
 export default async function TicketList() {
-  
   const tickets = await getTickets();
 
   return (
     <>
       {tickets.map((ticket) => (
-        <Link href={`/tickets/${ticket.id}`}>
-          <div key={ticket.id} className="card my-5">
+        <div key={ticket.id} className="card my-5">
+          <Link href={`/tickets/${ticket.id}`}>
             <h3>{ticket.title}</h3>
             <p>{ticket.body.slice(0, 200)}...</p>
             <div className={`pill ${ticket.priority}`}>
               {ticket.priority} priority
             </div>
-          </div>
-        </Link>
+          </Link>
+        </div>
       ))}
       {tickets.length === 0 && (
         <p className="text-center">There are no open tickets. yay!</p>
