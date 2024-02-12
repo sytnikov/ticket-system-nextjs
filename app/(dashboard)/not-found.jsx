859281309f53841fefb5b89cd242b417c08b508d
sytnikov <sytnikov.alexey@gmail.com>
2024-02-12 16:36:56 +0200

    added login and signup pages, added route groups

--- app/(dashboard)/not-found.jsx
+++ app/(dashboard)/not-found.jsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <main className="text-center">
       <h2 className="text-3xl">There was a problem :/</h2>
-      <p>We couldn't find the page you were looking for.</p>
+      <p>We could not find the page you were looking for.</p>
       <p>Go back to the <Link href="/">Dashboard</Link>.</p> 
     </main>
   )
