816be1110736f68a2da7d693e7b081ad00fc60cb
sytnikov <sytnikov.alexey@gmail.com>
2024-02-15 18:33:25 +0200

    authentication using cookies is added (sign up and log out)

--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,7 +1,44 @@
+"use client"
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import AuthForm from "../AuthForm";
+
+
 export default function Signup() {
+  const [formError, setFormError] = useState("")
+  const router = useRouter()
+
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault()
+
+    const supabase = createClientComponentClient()
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/api/auth/callback`
+      }
+    })
+
+    if (error) {
+      setFormError(error.message)
+    }
+
+    if (!error) {
+      router.push("/verify")
+    }
+  }
+  
   return (
     <main>
       <h2 className="text-center">Sign up</h2>
+      <AuthForm handleSubmit={handleSubmit}/>
+      {formError && (
+        <div className="error">{formError}</div>
+      )}
     </main>
   )
 }
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 
 import Logo from "../components/ticket-me.png"
+import LogoutButton from "./LogoutButton";
 
-export default function Navbar() {
+export default function Navbar( {user} ) {
   return (
     <nav>
       <Image 
@@ -15,7 +16,9 @@ export default function Navbar() {
       />
       <h1>TICKET.ME</h1>
       <Link href="/">Dashboard</Link>
-      <Link href="/tickets">Tickets</Link>
+      <Link href="/tickets" className="mr-auto">Tickets</Link>
+      {user && <span>Hello, {user.email}</span>}
+      <LogoutButton />
     </nav>
   );
 }
