19da971262c543a55674fe6b6f4fbfac4cc685b1
sytnikov <sytnikov.alexey@gmail.com>
2024-02-18 22:48:46 +0200

    reusable SubmitButton component added using useFormStatus hook

---
+++ app/components/SubmitButton.jsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { useFormStatus } from "react-dom"
+
+export default function SubmitButton() {
+  const { pending } = useFormStatus()
+  
+  return (
+    <button className="btn-primary" disabled={pending}>
+      {pending ? <span>Submitting...</span> : <span>Submit</span>}
+    </button>
+  )
+}
