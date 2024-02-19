a73b78bd7c3619a162dd78e1e80fbfdaf5b8d424
sytnikov <sytnikov.alexey@gmail.com>
2024-02-19 14:39:09 +0200

    test in a dev branch - added a card to Dashboard

--- app/(dashboard)/page.jsx
+++ app/(dashboard)/page.jsx
@@ -15,6 +15,10 @@ export default function Home() {
       <h2>Company Updates</h2>
 
       <div className="card">
+        <h3>This is a website deployed from a pull request (dev branch)</h3>
+        <p>Lorem ipsum possimus quam soluta rerum illo laborum ullam pariatur molestiae, modi beatae corrupti, assumenda distinctio adipisci, cupiditate minima eum vitae? Similique dicta est facilis debitis, autem temporibus quo repellat illum unde id iste veritatis eveniet, aspernatur enim quas.</p>
+      </div>
+      <div className="card">
         <h3>New accounting module updated</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, at quam. Dolores omnis possimus quam soluta rerum illo laborum ullam pariatur molestiae, modi beatae corrupti.</p>
       </div>
