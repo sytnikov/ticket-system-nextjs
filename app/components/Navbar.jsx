0ba42dbfc64a651a8d9e7b02930886485c53133d
sytnikov <sytnikov.alexey@gmail.com>
2024-02-20 22:37:39 +0200

    added sections to navbar to make it responsive

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,24 +1,33 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import Logo from "../components/ticket-me.png"
+import Logo from "../components/ticket-me.png";
 import LogoutButton from "./LogoutButton";
 
-export default function Navbar( {user} ) {
+export default function Navbar({ user }) {
   return (
     <nav>
-      <Image 
+      <Image
         src={Logo}
         alt="Ticket.me logo"
         width={70}
         quality={100}
         placeholder="blur"
+        className="flex"
       />
       <h1>TICKET.ME</h1>
-      <Link href="/">Dashboard</Link>
-      <Link href="/tickets" className="mr-auto">Tickets</Link>
-      {user && <span>Hello, {user.email}</span>}
-      <LogoutButton />
+      <div className="grow">
+        <div className="flex item-center justify-center gap-2 md:gap-4">
+          <Link href="/">Dashboard</Link>
+          <Link href="/tickets" className="mr-auto">
+            Tickets
+          </Link>
+        </div>
+      </div>
+      <div className="flex item-center justify-center gap-2"> 
+        {user && <span>Hello, {user.email}</span>}
+        <LogoutButton />
+      </div>
     </nav>
   );
 }
